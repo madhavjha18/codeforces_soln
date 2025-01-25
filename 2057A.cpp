5f9f555a7027fb163bf095e81537c2c1d24b979d
madhavjha18 <madhavjha18557@gmail.com>
2025-01-25 11:09:10 +0530

    MEX TABLE 2057A.cpp

---
+++ 2057A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int m;
+        cin>>m;
+        int ans;
+        if(n>m)
+        ans=n+1;
+        else
+        ans=m+1;
+        cout<<ans<<endl;
+        }
+      return 0;  
+    }
+    
